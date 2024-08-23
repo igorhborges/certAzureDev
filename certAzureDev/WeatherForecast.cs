304b470cacbf14af5f9d09790e9e5fb1743d0a0a
igorhborges <igorhenriqueborges@gmail.com>
2024-08-23 07:36:03 +0100

    Update WeatherForecast.cs

--- certAzureDev/WeatherForecast.cs
+++ certAzureDev/WeatherForecast.cs
@@ -6,7 +6,7 @@ namespace certAzureDev
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF => 31 + 1 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF => 30 + 2 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
     }
